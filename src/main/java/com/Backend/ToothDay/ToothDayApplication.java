0672b7a6c3cbb15c2eef9b8f3141a8a3a8977b26
shin-jae-eun <sshinjaeeunn@gmail.com>
2024-07-04 16:38:11 +0900

    First Commit

---
+++ src/main/java/com/Backend/ToothDay/ToothDayApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.ToothDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToothDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToothDayApplication.class, args);
+	}
+
+}
