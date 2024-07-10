1679d7cb584d30c26c1eb392106536030b301f51
chaelink <lin1041840@gmail.com>
2024-07-11 08:55:20 +0900

    Domain update

--- src/main/java/com/Backend/ToothDay/community/like/PostLike.java
+++ src/main/java/com/Backend/ToothDay/community/like/PostLike.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Entity
 public class PostLike {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "likeId")
     private int likeId;
 
