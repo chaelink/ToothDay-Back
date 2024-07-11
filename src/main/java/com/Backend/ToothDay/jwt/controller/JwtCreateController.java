ab5b13b825f3d5aa1b171c5d0a93288a4b142693
shin-jae-eun <sshinjaeeunn@gmail.com>
2024-07-11 23:12:28 +0900

    User Entity에 profileurl 추가

--- src/main/java/com/Backend/ToothDay/jwt/controller/JwtCreateController.java
+++ src/main/java/com/Backend/ToothDay/jwt/controller/JwtCreateController.java
@@ -43,7 +43,8 @@ public class JwtCreateController {
 
         if(userEntity == null) {
             User userRequest = User.builder()
-                    .username(googleUser.getProvider()+"_"+googleUser.getProviderId())
+                    .username(googleUser.getName())
+                    .profileImageUrl(googleUser.getProfileImageUrl())
                     .email(googleUser.getEmail())
                     .provider(googleUser.getProvider())
                     .providerId(googleUser.getProviderId())
