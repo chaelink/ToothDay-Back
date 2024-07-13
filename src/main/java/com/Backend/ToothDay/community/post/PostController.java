44c58670e16eb499e193512d1bd79fccc3c3b68e
shin-jae-eun <sshinjaeeunn@gmail.com>
2024-07-13 22:14:38 +0900

    Aplly changes from jaeeun

--- src/main/java/com/Backend/ToothDay/community/post/PostController.java
+++ src/main/java/com/Backend/ToothDay/community/post/PostController.java
@@ -27,10 +27,12 @@ public class PostController {
         return postService.getAllPostDTO();
     }
 
-    @GetMapping("/community/{keywordId}") //게시글 조회
-    public List<PostDTO> communityFindByKeywordId(@PathVariable int keywordId) {
-        return postService.getPostDTOByKeywordId(keywordId);
-    }
+//    @GetMapping("/community/{keywordId}")
+//    public List<PostDTO> communityFindByKeywordId(@PathVariable int keywordId) {
+//
+//    }
+
+
 
     @GetMapping("/community/upload") //커뮤니티 작성 화면
     public PostForm communityUploadForm() {
@@ -38,7 +40,6 @@ public class PostController {
     }
 
     @PostMapping("/community/upload") //게시글 작성
-
     public PostDTO communityUpload(@RequestBody PostForm postForm, HttpServletRequest request) {
         // JWT 토큰에서 userId 추출
         String token = request.getHeader("Authorization").replace("Bearer ", "");
@@ -46,6 +47,7 @@ public class PostController {
 
         // userId를 이용하여 User 객체를 가져옵니다.
         User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
 
         Post post = new Post();
         post.setTitle(postForm.getTitle());
