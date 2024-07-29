f70c231ce9d6eb0af88d4b4775dd9b6159616943
shin-jae-eun <129717192+shin-jae-eun@users.noreply.github.com>
2024-07-29 18:26:32 +0900

    Update JwtProperties.java

--- src/main/java/com/Backend/ToothDay/jwt/config/jwt/JwtProperties.java
+++ src/main/java/com/Backend/ToothDay/jwt/config/jwt/JwtProperties.java
@@ -1,8 +1,8 @@
 package com.Backend.ToothDay.jwt.config.jwt;
 
 public interface JwtProperties {
-    String SECRET = "swi3LqRnKfHz7gVl0w9jK!2Qp#T@M1cRf5x%YzPbW8v&UkEo"; // 더 복잡한 비밀값
-    int EXPIRATION_TIME = 7200000; // 2시간 (1/1000초 단위)
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
+    String SECRET = ""; 
+    int EXPIRATION_TIME = ;
+    String TOKEN_PREFIX = "";
+    String HEADER_STRING = "";
 }
