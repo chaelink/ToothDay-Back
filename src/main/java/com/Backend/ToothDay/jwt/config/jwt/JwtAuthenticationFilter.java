85a107cc2c37713c549814de9d571537d4a4e1cf
shin-jae-eun <sshinjaeeunn@gmail.com>
2024-07-29 23:59:56 +0900

    jwt + 재로그인 시 최신 정보로 조회 22

--- src/main/java/com/Backend/ToothDay/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/Backend/ToothDay/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,5 @@
 package com.Backend.ToothDay.jwt.config.jwt;
+
 import java.io.IOException;
 import java.util.Date;
 
@@ -6,7 +7,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import com.Backend.ToothDay.jwt.config.auth.PrincipalDetails;
 import com.Backend.ToothDay.jwt.dto.LoginRequestDto;
@@ -28,17 +28,12 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
     private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository; // UserRepository 추가
+    private final UserRepository userRepository;
 
-    // Authentication 객체 만들어서 리턴 => 의존 : AuthenticationManager
-    // 인증 요청시에 실행되는 함수 => /login
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
 
-        System.out.println("JwtAuthenticationFilter : 진입");
-
-        // request에 있는 username과 password를 파싱해서 자바 Object로 받기
         ObjectMapper om = new ObjectMapper();
         LoginRequestDto loginRequestDto = null;
         try {
@@ -47,34 +42,17 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             e.printStackTrace();
         }
 
-//        System.out.println("JwtAuthenticationFilter : "+loginRequestDto);
-
-        // 유저네임패스워드 토큰 생성
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(
                         loginRequestDto.getUsername(),
                         loginRequestDto.getPassword());
 
-//        System.out.println("JwtAuthenticationFilter : 토큰생성완료");
-
-        // authenticate() 함수가 호출 되면 인증 프로바이더가 유저 디테일 서비스의
-        // loadUserByUsername(토큰의 첫번째 파라메터) 를 호출하고
-        // UserDetails를 리턴받아서 토큰의 두번째 파라메터(credential)과
-        // UserDetails(DB값)의 getPassword()함수로 비교해서 동일하면
-        // Authentication 객체를 만들어서 필터체인으로 리턴해준다.
-
-        // Tip: 인증 프로바이더의 디폴트 서비스는 UserDetailsService 타입
-        // Tip: 인증 프로바이더의 디폴트 암호화 방식은 BCryptPasswordEncoder
-        // 결론은 인증 프로바이더에게 알려줄 필요가 없음.
         Authentication authentication =
                 authenticationManager.authenticate(authenticationToken);
 
-        PrincipalDetails principalDetailis = (PrincipalDetails) authentication.getPrincipal();
-//        System.out.println("Authentication : "+principalDetailis.getUser().getUsername());
         return authentication;
     }
 
-    // JWT Token 생성해서 response에 담아주기
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
@@ -90,6 +68,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
                 .withClaim("id", userEntity.getId())
                 .withClaim("username", userEntity.getUsername())
+                .withClaim("profileImageUrl", userEntity.getProfileImageUrl()) // 프로필 이미지 URL 추가
                 .sign(Algorithm.HMAC512(JwtProperties.SECRET));
 
         response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
