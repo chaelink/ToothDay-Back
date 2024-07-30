66ab7de471b71def37e747e736f7d8a3c7474678
shin-jae-eun <sshinjaeeunn@gmail.com>
2024-07-30 15:48:30 +0900

    스웨거 jwt토큰 발급 시 null 발생 오류 해결

--- src/main/java/com/Backend/ToothDay/jwt/dto/UserDTO.java
+++ src/main/java/com/Backend/ToothDay/jwt/dto/UserDTO.java
@@ -11,16 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class UserDTO {
-
     private long id;
     private String username;
     private String profileImageUrl;
     private String email;
 
     public static UserDTO from(User user) {
+        if (user == null) {
+            return null;
+        }
         return UserDTO.builder()
                 .id(user.getId())
-                .username(user.getUsername())
+                .username(user.getUsername()) // 이 부분에서 null 체크를 할 수도 있습니다.
                 .profileImageUrl(user.getProfileImageUrl())
                 .email(user.getEmail())
                 .build();
